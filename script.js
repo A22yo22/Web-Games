0c1304d9ec93adc4065ea46839d11ddae708d0ee
A22yo22 <a22yo22io@gmail.com>
2022-06-29 18:59:27 +0200

    loser  ha ha

--- script.js
+++ script.js
@@ -15,4 +15,13 @@ function jump() {
 var checkDead = setInterval(function(){
 
     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+
+    if(blockLeft < 20 && blockLeft > 0 && characterTop >= 130) {
+        block.style.animation = "none";
+        block.style.display = "none";
+
+        alert("you lose.")
+    }
 },10);
