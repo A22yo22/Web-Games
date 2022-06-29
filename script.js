aa2f31dc1c14ac81003d40da511b1f02256718d6
A22yo22 <a22yo22io@gmail.com>
2022-06-29 18:56:33 +0200

    jump

--- script.js
+++ script.js
@@ -7,8 +7,12 @@ function jump() {
         character.classList.add("animate");
     }
 
-    
-    setTimeout(function(){
+    setTimeout(function() {
         character.classList.remove("animate");
     },500);
 }
+
+var checkDead = setInterval(function(){
+
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+},10);
